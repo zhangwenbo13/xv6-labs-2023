ce17f0f460f67f80b1da1acc25181d89ab88fae2
Tim Zhang <hgmz371@gmail.com>
2023-04-23 11:47:46 +0000

    lab2: System call tracing

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
 
   np->state = RUNNABLE;
 
+  np->mask = p->mask;
+
   release(&np->lock);
 
   return pid;
@@ -692,4 +694,13 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+int
+trace(int mask)
+{
+  struct proc *p = myproc();
+  p->mask = mask;
+  
+  return 0;  
 }
